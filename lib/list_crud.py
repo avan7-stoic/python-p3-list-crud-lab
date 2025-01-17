fdafba2a2812e4d1de7a4571c66f0e6347ca5bf1
avant muchiri <avant.muchiri@student.moringaschool>
2025-01-17 03:02:09 +0300

    done

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -9,20 +9,32 @@ def add_element_to_end_of_list(my_list, element):
     my_list.append(element)
     return my_list
 
-def add_element_to_start_of_list(l, element):
-    return None
+def add_element_to_start_of_list(my_element, element):
+    my_element.insert(0, element)
+    return my_element
 
-def remove_element_from_end_of_list(l):
-    return None
+def remove_element_from_end_of_list(my_list):
+    my_list.pop()
+    return my_list
 
-def remove_element_from_start_of_list(l):
-    return None
+def remove_element_from_start_of_list(my_list):
+    if my_list:
+        del my_list[0]
+    return my_list
 
-def retrieve_first_element_from_list(l):
-    return None
+def retrieve_first_element_from_list(my_list):
+    if my_list:
+        return my_list[0]
+    else:
+        return None
+def retrieve_element_from_index(my_index, index):
+    if 0 <= index < len(my_index): 
+        return my_index[index]
+    else:
+        return None 
 
-def retrieve_element_from_index(l, index):
-    return None
-
-def retrieve_last_element_from_list(l):
-    return None
+def retrieve_last_element_from_list(my_list):
+    if my_list:
+        return my_list[-1]
+    else:
+        return None
